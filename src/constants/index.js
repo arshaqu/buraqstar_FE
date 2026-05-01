b45dd7909359d360fc3cb679aaeb1d0d404ffde5
buraqstar <buraquiux@gmail.com>
2026-05-01 15:58:53 +0400

    small update

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,17 +3,17 @@ import { MapStyle } from "./googlemapstyle";
 // Base URL
 export const ImageURL = 'https://buraq.frappe.cloud';
 // export const BASE_URL = 'https://buraqstar.digitaezonline.com';
-// export const BASE_URL = 'https://admin.buraqstar.com';-------
+export const BASE_URL = 'https://admin.buraqstar.com';
 // export const BASE_URL = 'http://buraq.local';
 // export const BASE_URL = 'http://127.0.0.1:8001';
 // export const BASE_URL = 'http://192.168.0.165:3366';
-export const BASE_URL = 'http://127.0.0.1:8000';
+// export const BASE_URL = 'http://127.0.0.1:8000';
 
 export const FREE_DELIVERY_THRESHOLD = 150;
 
 // Site URL for SEO and frontend
-// export const SITE_URL = 'https://buraqstar.com';----------------
-export const SITE_URL = 'http://localhost:3000'; // Development
+export const SITE_URL = 'https://buraqstar.com';
+// export const SITE_URL = 'http://localhost:3000'; // Development
 
 export const REACT_APP_GOOGLE_MAPS_API_KEY="AIzaSyBklxYBuqfHt6UD_mHtjp9zacwaVWhldZA"
 export const GoogleMapStyle =  MapStyle
